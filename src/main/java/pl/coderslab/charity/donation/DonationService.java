4b77fcb80caaa7a250de4230c16d24951be01310
Bartosz <b.redkowiak@gmail.com>
2019-11-26 23:05:49 +0100

    Added donation counter to index view

--- src/main/java/pl/coderslab/charity/donation/DonationService.java
+++ src/main/java/pl/coderslab/charity/donation/DonationService.java
@@ -1,9 +1,21 @@
 package pl.coderslab.charity.donation;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 public class DonationService {
+
+    private final DonationInterface donationInterface;
+    @Autowired
+    public DonationService(DonationInterface donationInterface) {
+        this.donationInterface = donationInterface;
+    }
+
+    public Long getTotalQuantity(){
+        return donationInterface.totalQuantity();
+    }
+
 }
