7f2acce039ae43e7d000a02dd82247a664599db3
Bartosz <b.redkowiak@gmail.com>
2019-11-26 22:51:29 +0100

    Added list of institutions to index view

--- src/main/java/pl/coderslab/charity/HomeController.java
+++ src/main/java/pl/coderslab/charity/HomeController.java
@@ -1,16 +1,25 @@
 package pl.coderslab.charity;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.charity.institution.InstitutionService;
 
 
 @Controller
 public class HomeController {
 
+    private final InstitutionService institutionService;
+
+    @Autowired
+    public HomeController(InstitutionService institutionService) {
+        this.institutionService = institutionService;
+    }
 
     @RequestMapping("/")
     public String homeAction(Model model){
+        model.addAttribute("institutions", institutionService.getFirst10());
         return "index";
     }
 }
--- src/main/java/pl/coderslab/charity/institution/InstitutionService.java
+++ src/main/java/pl/coderslab/charity/institution/InstitutionService.java
@@ -1,9 +1,28 @@
 package pl.coderslab.charity.institution;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional
 public class InstitutionService {
+
+    private final InstitutionRepository institutionRepository;
+
+    @Autowired
+    public InstitutionService(InstitutionRepository institutionRepository) {
+        this.institutionRepository = institutionRepository;
+    }
+
+    public List<Institution> getAll(){
+        return institutionRepository.findAll();
+    }
+
+    public List<Institution> getFirst10(){
+        return institutionRepository.findFirst10ByOrderByIdAsc();
+    }
+
 }
