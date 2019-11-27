53518190134109a314c0d861959b17f1ba4ea8fe
Bartosz <b.redkowiak@gmail.com>
2019-11-27 17:56:20 +0100

    Temporary sql dumps

---
+++ src/main/resources/sql/charity_donation_category.sql
@@ -0,0 +1,5 @@
+INSERT INTO charity_donation.category (id, name) VALUES (1, 'ubrania, które nadają się do ponownego użycia');
+INSERT INTO charity_donation.category (id, name) VALUES (2, 'ubrania, do wyrzucenia');
+INSERT INTO charity_donation.category (id, name) VALUES (3, 'zabawki');
+INSERT INTO charity_donation.category (id, name) VALUES (4, 'książki');
+INSERT INTO charity_donation.category (id, name) VALUES (5, 'inne');
---
+++ src/main/resources/sql/charity_donation_institution.sql
@@ -0,0 +1,4 @@
+INSERT INTO charity_donation.institution (id, description, name) VALUES (1, 'Pomoc dzieciom z ubogich rodzin', 'Dbam o zdrowie');
+INSERT INTO charity_donation.institution (id, description, name) VALUES (2, 'Pomoc osobom znajdującym się w trudnej sytuacji życiowej', 'Dla dzieci');
+INSERT INTO charity_donation.institution (id, description, name) VALUES (3, 'Pomoc w wybudzaniu dzieci ze śpiączki', 'A kogo');
+INSERT INTO charity_donation.institution (id, description, name) VALUES (4, 'Pomoc dla osób nie posiadających miejsca zamieszkania', 'Bez domu');
